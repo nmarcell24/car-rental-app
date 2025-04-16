8530909716ef5d171dfac534f5cec6da708cdd54
ZsoltMandrusz <zsoltmandrusz@gmail.com>
2025-04-16 14:34:52 +0100

    Backend kész

--- backend/src/main/resources/db/changelog/insert_dummy_allocate.sql
+++ backend/src/main/resources/db/changelog/insert_dummy_allocate.sql
@@ -4,4 +4,9 @@ INSERT INTO allocate( user_id, permission_id) VALUES
     (1, 'LIST_LOANS'),
     (1, 'CREATE_LOAN'),
     (1, 'CREATE_CAR'),
-    (1, 'DELETE_CAR');
+    (1, 'DELETE_CAR'),
+    (2, 'UPDATE_USER'),
+    (2, 'READ_USER'),
+    (2, 'LIST_LOANS'),
+    (2, 'CREATE_LOAN'),
+    (2, 'CREATE_CAR');
