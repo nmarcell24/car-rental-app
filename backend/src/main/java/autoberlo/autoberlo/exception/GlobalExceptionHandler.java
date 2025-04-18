9c5a29422c84c72b4d0c560161bdf3f11c2f78ce
ZsoltMandrusz <zsoltmandrusz@gmail.com>
2025-04-18 15:07:32 +0100

    swagger kész

--- backend/src/main/java/autoberlo/autoberlo/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/autoberlo/autoberlo/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package autoberlo.autoberlo.exception;
 
-import autoberlo.autoberlo.exception.LoanAlreadyExcistsException;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
